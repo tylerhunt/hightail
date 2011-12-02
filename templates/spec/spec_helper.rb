83201dec70ca1d5b55781a45c6cba9dc22e1b5e5
Tyler Hunt <tyler@tylerhunt.com>
2011-12-01 23:04:48 -0800

    Bring spec helper template up to date.

--- templates/spec/spec_helper.rb
+++ templates/spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'rspec/autorun'
 
 # Requires supporting files with custom matchers and macros, etc.
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
@@ -10,7 +11,7 @@ RSpec.configure do |config|
   config.mock_with :rspec
 <% unless options[:skip_active_record] -%>
   config.fixture_path = Rails.root.join('spec', 'fixtures')
-  config.use_transactional_examples = false
+  config.use_transactional_fixtures = true
 <% end -%>
   config.infer_base_class_for_anonymous_controllers = false
 end
